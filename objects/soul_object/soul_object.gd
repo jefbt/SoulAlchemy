a09085a7219d7d787a80ac0e34aa32532b9008ba
Jeferson Tomazella <jefbt@outlook.com>
2024-08-15 11:50:29 -0300

    Starting Commit: Adding the existing files to the git project

---
+++ objects/soul_object/soul_object.gd
@@ -0,0 +1,108 @@
+class_name SoulObject
+extends Area2D
+
+enum Element { Water = 0, Metal = 1, Earth = 2, Fire = 3, Wood = 4 }
+enum Type { Queue = 0, Falling = 1, Launch = -1 }
+
+const ACCELERATION = 500.0
+@onready var sprite = $Sprite
+@onready var sprite_light = $SpriteLight
+@onready var sprite_shadows = $SpriteShadows
+
+@onready var top_conn = $Connections/TopConn
+@onready var bottom_conn = $Connections/BottomConn
+@onready var left_conn = $Connections/LeftConn
+@onready var right_conn = $Connections/RightConn
+
+@onready var label_row = $Connections/LabelRow
+@onready var label_lane = $Connections/LabelLane
+
+@export var element_color: Array[Color] = [
+	Color.hex(0x5DE2E7FF),
+	Color.hex(0xCECECEFF),
+	Color.hex(0x8D6F64FF),
+	Color.hex(0xFF5601FF),
+	Color.hex(0xBFD641FF),
+]
+@export var element : Element:
+	set(value):
+		element = value
+		sprite.animation = str(int(element))
+		sprite.modulate = element_color[int(value)]
+@export var type : Type = Type.Queue
+@export var is_random_element : bool = true
+
+var lane : int = -1
+var row : int = -1
+var is_falling := false
+var launched := false
+var delta_move := 0.0
+var move_duration := 0.5
+var start_y := 0.0
+var next_y := 0.0
+var launch_speed := 50.0
+var check_function : Callable
+var top : SoulObject = null
+var left : SoulObject = null
+var right : SoulObject = null
+var bottom : SoulObject = null
+var grid_size := 0.0
+var game_manager: GameManager = null
+var counted := false
+
+func _ready():
+	if is_random_element:
+		var element_index = randi() % Element.size()
+		element = Element.values()[element_index]
+
+func _process(delta):
+	sprite_light.visible = game_manager.mode == GameManager.Mode.Light
+	sprite_shadows.visible = game_manager.mode == GameManager.Mode.Shadows
+	if is_falling:
+		delta_move += delta / move_duration
+		position.y = lerpf(start_y, next_y, delta_move)
+		if delta_move >= 1:
+			is_falling = false
+			position.y = next_y
+		if position.y >= GameManager.bottom_line:
+			hit_bottom()
+	elif launched:
+		position.y -= launch_speed * delta
+		launch_speed += delta * ACCELERATION
+		if position.y < -400: dismiss()
+	top_conn.visible = top != null
+	bottom_conn.visible = bottom != null
+	left_conn.visible = left != null
+	right_conn.visible = right != null
+	label_row.text = str(row)
+	label_lane.text = str(lane)
+
+func _on_area_entered(area):
+	if type != Type.Launch: return
+	var other = area as SoulObject
+	if other == null or other.type != Type.Falling: return
+	if other.lane != lane: return
+	check_function.call(other, self)
+
+func move(move_amount: float, duration: float = 0):
+	if type == Type.Falling:
+		is_falling = true
+		delta_move = 0.0
+		move_duration = duration
+		if move_duration <= 0: move_duration = 0.001
+		start_y = position.y
+		next_y = position.y + move_amount
+		#await get_tree().create_timer(duration).timeout
+		#is_falling = false
+	elif type == Type.Launch:
+		launch_speed = move_amount
+		launched = true
+
+func dismiss():
+	queue_free()
+
+func hit_bottom():
+	if game_manager != null:
+		game_manager.trigger_game_over(self)
+	else:
+		print("No GameMamanger set. 'hit_bottom()' did nothing")
---
+++ scripts/game_manager.gd
@@ -0,0 +1,549 @@
+class_name GameManager
+extends Node
+
+# TODO: find out about showing ads on android version (and maybe web version)
+# TODO: when we hit an element, pop all the same elements above that, and score
+	# for all of them
+
+enum Mode { Light = 0, Shadows = 1 }
+enum MoveDirection { Left = -1, Right = 1 }
+
+const SOUL_OBJECT = preload("res://objects/soul_object/soul_object.tscn")
+@onready var soul_objects = $"SoulObjects"
+@onready var mode_node = $UI/UIPosition/Mode
+@onready var light = $UI/Light
+@onready var shadows = $UI/Shadows
+@onready var mode_shadows = $UI/ModeShadows
+@onready var mode_light = $UI/ModeLight
+@onready var auto_launch_off = $UI/AutoLaunchOff
+@onready var auto_launch_on = $UI/AutoLaunchOn
+@onready var score_light = $UI/Score/ScoreLight
+@onready var score_shadows = $UI/Score/ScoreShadows
+@onready var multiplier = $UI/Score/Multiplier
+@onready var score_balance = $UI/Score/Balance/ScoreBalance
+
+@onready var target_sprite_0 = $Target/TargetSprite0
+@onready var target_sprite_1 = $Target/TargetSprite1
+@onready var target_sprite_2 = $Target/TargetSprite2
+@onready var target_sprite_3 = $Target/TargetSprite3
+@onready var target_sprite_4 = $Target/TargetSprite4
+
+@onready var multi_timer = $UI/Score/Multiplier/MultiTimer
+
+@export var grid_size : float = 65.0
+@export var grid_count_x : int = 5
+@export var grid_count_y : int = 7
+@export var falling_move_duration = 0.5
+@export var launch_speed = 128
+@export var max_queue = 3
+@export var base_score = 1.0
+@export var score_multiplier_increment = 0.25
+@export var max_score_multiplier = 3.0
+
+@export var drop_symbols: Array[Node2D]
+var targets : Array[Sprite2D]
+
+var falling_objects : Dictionary
+var queue_objects : Array[SoulObject]
+var launched_objects = { 0: null, 1: null, 2: null, 3: null, 4: null }
+var selected_object : SoulObject
+var launch_lane : int = -1
+var starting_launch_lane = 2
+
+var falling_start_x = 0.0
+var falling_start_y = 0.0
+var launch_y = 0
+var queue_vector = Vector2(1, 0)
+var queue_start = Vector2(215, 225.5)
+var input_modifier := false
+var auto_launch := false
+
+var drop := 0
+var reseting_drop := false
+var drop_reset_time := 0.25
+var next_drop_reset := -1.0
+var drop_reset_count := 0
+
+var balance_score := 0
+var light_score := 0
+var shadows_score := 0
+var score_multiplier := 1.0
+var reduce_multiplier_time := -1.0
+var reduce_delta := 0.0
+var reset_delta := false
+
+const REDUCE_MULTIPLIER_DURATION = 6.0
+
+static var mode : Mode = Mode.Light
+static var bottom_line : float = 0.0
+
+# built-in functions
+func _ready():
+	var size_x = grid_size * grid_count_x
+	var size_y = grid_size * grid_count_y
+	falling_start_x = -size_x / 2.0 + grid_size / 2
+	falling_start_y = -size_y / 2.0 - grid_size
+	launch_y = size_y / 2.0 - grid_size
+	bottom_line = launch_y
+	targets = [
+		target_sprite_0,
+		target_sprite_1,
+		target_sprite_2,
+		target_sprite_3,
+		target_sprite_4,
+	]
+	change_mode(mode)
+	set_auto_launch(auto_launch)
+	add_falling_line()
+	add_falling_line()
+	start_queue()
+	update_drop_symbols()
+	score_update()
+	update_target_sprite()
+
+func _process(delta):
+	input_modifier = Input.is_action_pressed("LaunchLaneTrigger")
+	check_drop_reseting()
+	check_score_multiplier(delta)
+# end built-int
+
+# management
+func trigger_game_over(soul_object: SoulObject = null):
+	var sname = "Quit"
+	if soul_object != null: sname = soul_object.name
+	print("GAME OVER | Trigger: " + sname)
+	get_tree().reload_current_scene() # make something better than this
+
+func change_mode(new_mode: Mode):
+	self.mode = new_mode
+	if self.mode == Mode.Light:
+		mode_light.visible = true
+		mode_shadows.visible = false
+		shadows.visible = false
+		light.visible = true
+	else:
+		mode_light.visible = false
+		mode_shadows.visible = true
+		shadows.visible = true
+		light.visible = false
+	update_target_sprite()
+
+func toggle_mode():
+	if mode == Mode.Light: change_mode(Mode.Shadows)
+	else: change_mode(Mode.Light)
+# end management
+
+# QUEUE region
+func start_queue():
+	launch_lane = starting_launch_lane
+	for n in max_queue: 
+		var so = SOUL_OBJECT.instantiate()
+		so.game_manager = self
+		add_queue(so as SoulObject, false)
+	queue_to_lane()
+	set_auto_launch(false)
+
+func add_queue(soul_object: SoulObject, update : bool = true):
+	if soul_object == null: return
+	if queue_objects.size() >= max_queue:
+		while queue_objects.size() > max_queue:
+			queue_objects.pop_back()
+		return
+	if queue_objects.has(soul_object): return
+	soul_object.type = SoulObject.Type.Queue
+	queue_objects.append(soul_object)
+	soul_objects.add_child(soul_object)
+	if update: update_queue()
+
+func remove_queue(soul_object: SoulObject, update : bool = true):
+	queue_objects.erase(soul_object)
+	if update: update_queue()
+
+func queue_to_lane():
+	if selected_object != null: return
+	if queue_objects == null or queue_objects.size() == 0: return
+	selected_object = queue_objects.pop_front() as SoulObject
+	selected_object.lane = launch_lane
+	var so = SOUL_OBJECT.instantiate()
+	so.game_manager = self
+	add_queue(so as SoulObject, false)
+	update_selected()
+	update_queue()
+
+func update_queue():
+	if queue_objects == null or queue_objects.size() == 0: return
+	for n in queue_objects.size():
+		var qo = queue_objects[n]
+		qo.position = queue_start + ((queue_vector * n) * grid_size)
+# end QUEUE
+
+# SELECTION region
+func update_selected():
+	update_target_sprite()
+	if selected_object == null: return
+	selected_object.position.y = launch_y + grid_size
+	selected_object.position.x = selected_object.lane * grid_size + falling_start_x
+
+func move_selected(direction):
+	if selected_object == null: return
+	var lane = selected_object.lane + int(direction)
+	if lane < 0: lane = 0
+	if lane >= grid_count_x: lane = grid_count_x - 1
+	selected_object.lane = lane
+	update_selected()
+# end SELECTION
+
+# FALLING region
+func update_drop_symbols():
+	for n in drop:
+		var node = drop_symbols[n] as Node2D
+		node.visible = true
+	for m in drop_symbols.size() - drop:
+		var i = drop + m
+		var node = drop_symbols[i] as Node2D
+		node.visible = false
+
+func reset_drop():
+	move_falling()
+	drop = 0
+	update_drop_symbols()
+
+func check_drop_reseting():
+	if reseting_drop:
+		if next_drop_reset <= 0.0:
+			next_drop_reset = Time.get_ticks_usec() + drop_reset_time + 0.5
+			return
+		if Time.get_ticks_usec() >= next_drop_reset:
+			next_drop_reset = Time.get_ticks_usec() + drop_reset_time
+			var node = drop_symbols[drop_reset_count] as Node2D
+			node.visible = false
+			drop_reset_count += 1
+		if drop_reset_count >= drop_symbols.size():
+			drop_reset_count = 0
+			next_drop_reset = -1.0
+			reset_drop()
+			reseting_drop = false
+
+func check_needs_falling():
+	#return # TODO: remove this
+	if reseting_drop: return
+	drop += 1
+	update_drop_symbols()
+	if drop >= drop_symbols.size(): reseting_drop = true
+
+func clean_up_falling():
+	for row in falling_objects.keys():
+		var is_null = true
+		for lane in falling_objects[row].values():
+			if lane != null:
+				is_null = false
+				break
+		if is_null:
+			falling_objects.erase(row)
+
+func move_falling():
+	# todo check if add a new line when moving
+	clean_up_falling()
+	add_falling_line()
+	for row in falling_objects.keys():
+		for lane in falling_objects[row].keys():
+			if falling_objects[row][lane] != null:
+				falling_objects[row][lane].move(grid_size, falling_move_duration)
+	# end move_falling
+
+func add_falling_line():
+	var line = {}
+	var b_line = null
+	var row = -1
+	var topmost_position = falling_start_y
+	for key in falling_objects.keys():
+		if row < key: 
+			row = key
+			for fo in falling_objects[key].values():
+				if fo != null:
+					topmost_position = fo.position.y - grid_size
+	if falling_objects.keys().has(row):
+		b_line = falling_objects[row]
+	row += 1
+	for lane in grid_count_x: line[lane] = null
+	for lane in grid_count_x:
+		var new_obj = SOUL_OBJECT.instantiate()
+		soul_objects.add_child(new_obj)
+		new_obj.game_manager = self
+		new_obj.type = SoulObject.Type.Falling
+		new_obj.lane = lane
+		new_obj.row = row
+		new_obj.check_function = check_clash
+		new_obj.position.x = lane * grid_size + falling_start_x
+		new_obj.position.y = topmost_position
+		if lane > 0: new_obj.left = line[lane - 1]
+		if b_line != null:
+			new_obj.bottom = b_line[lane]
+			if b_line[lane] != null:
+				b_line[lane].top = new_obj
+		line[lane] = new_obj
+	falling_objects[row] = line
+	for lane in grid_count_x:
+		if lane < grid_count_x - 1:
+			line[lane].right = line[lane + 1]
+# end FALLING
+
+# LAUCH region
+func launch_selected():
+	if selected_object == null: return
+	if launched_objects[selected_object.lane] != null: return
+	launch_obj(selected_object)
+	launch_lane = selected_object.lane
+	selected_object = null
+	queue_to_lane()
+
+func launch_obj(obj: SoulObject):
+	if obj == null: return
+	if launched_objects[obj.lane] != null: return
+	obj.type = SoulObject.Type.Launch
+	obj.check_function = check_clash
+	obj.position.x = obj.lane * grid_size + falling_start_x
+	obj.position.y = launch_y
+	obj.move(launch_speed)
+	launched_objects[obj.lane] = obj
+	check_needs_falling()
+
+func add_launch(lane: int):
+	if launched_objects[lane] != null: return
+	if lane < 0: lane = 0
+	if lane >= grid_count_x: lane = grid_count_x - 1
+	var new_obj = SOUL_OBJECT.instantiate()
+	new_obj.game_manager = self
+	new_obj.lane = lane
+	soul_objects.add_child(new_obj)
+	launch_obj(new_obj)
+
+func mode_direction():
+	if mode == Mode.Light: return 1
+	else: return -1
+
+func check_clash(falling: SoulObject, launch: SoulObject):
+	var direction = mode_direction()
+	var wanted = int(falling.element) - direction
+	if wanted < 0: wanted = SoulObject.Element.size() - 1
+	if wanted >= SoulObject.Element.size(): wanted = 0
+	var passed = int(launch.element) == wanted
+	if passed: match_elements(falling, launch)
+	else: wrong_elements(falling, launch)
+
+func find_extra_match(soul_obj: SoulObject, element) -> int:
+	if soul_obj == null or soul_obj.counted: return 0
+	if not (soul_obj is SoulObject): return 0
+	if soul_obj.element != element: return 0
+	soul_obj.counted = true
+	var count = 0
+	if soul_obj.left != null and not soul_obj.left.counted:
+		count += find_extra_match(soul_obj.left, element)
+	if soul_obj.right != null and not soul_obj.right.counted:
+		count += find_extra_match(soul_obj.right, element)
+	if soul_obj.top != null and not soul_obj.top.counted:
+		count += find_extra_match(soul_obj.top, element)
+	if soul_obj.bottom != null and not soul_obj.bottom.counted:
+		count += find_extra_match(soul_obj.bottom, element)
+	soul_obj.dismiss()
+	return count + 1
+
+func match_elements(falling: SoulObject, launch: SoulObject):
+	var extra = find_extra_match(falling, falling.element)
+	var extra_multi = 1.0 + float(extra) / 5.0
+	var extra_score = int(float(extra) * extra_multi)
+	score(extra_score + 1)
+	falling.dismiss()
+	launch.dismiss()
+	
+func wrong_elements(falling: SoulObject, launch: SoulObject):
+	launched_objects[launch.lane] = null
+	launch.launched = false
+	launch.type = SoulObject.Type.Falling
+	launch.position.y = falling.position.y + grid_size
+	launch.grid_size = grid_size
+	var row = falling.row - 1
+	if !falling_objects.keys().has(row):
+		falling_objects[row] = {}
+		for n in grid_count_x:
+			falling_objects[row][n] = null
+	falling_objects[row][launch.lane] = launch
+	falling.bottom = launch
+	launch.top = falling
+	launch.row = row
+	for lane in falling_objects[row].keys():
+		if falling_objects[row][lane] == null: continue
+		if lane > 0:
+			falling_objects[row][lane].left = falling_objects[row][lane - 1]
+		if lane < grid_count_x - 1:
+			falling_objects[row][lane].right = falling_objects[row][lane + 1]
+	if launch.position.y >= bottom_line:
+		launch.hit_bottom()
+	wrong()
+# end LAUNCH
+
+# extra
+func update_target_sprite():
+	for ts in targets:
+		var tar = ts as Sprite2D
+		if tar != null:
+			tar.visible = false
+	if selected_object != null:
+		var direction = mode_direction()
+		var wanted = int(selected_object.element) + direction
+		if wanted < 0: wanted = SoulObject.Element.size() - 1
+		if wanted >= SoulObject.Element.size(): wanted = 0
+		var color = selected_object.element_color[wanted]
+		var tar = targets[wanted] as Sprite2D
+		if tar != null:
+			tar.modulate = color
+			tar.visible = true
+
+func free_soul_objects():
+	for qo in queue_objects:
+		if qo != null: qo.queue_free()
+	for lo in launched_objects.values():
+		if lo != null and lo is SoulObject: lo.queue_free()
+	for fo in falling_objects.values():
+		if fo == null: continue
+		if fo is Array:
+			for ofo in fo: if ofo != null: ofo.queue_free()
+		elif fo is Dictionary:
+			for ofo in fo.values(): if ofo != null: ofo.queue_free()
+		else:
+			fo.queue_free()
+	falling_objects.clear()
+	launched_objects.clear()
+	queue_objects.clear()
+	if selected_object != null: selected_object.queue_free()
+	launched_objects = { 0: null, 1: null, 2: null, 3: null, 4: null }
+
+func score_update():
+	score_light.text = str(light_score)
+	score_shadows.text = str(shadows_score)
+	multiplier.text = str(int(score_multiplier))
+	score_balance.text = str(balance_score)
+
+func calc_balance_score():
+	var diff = abs(light_score - shadows_score)
+	var best = max(light_score, shadows_score)
+	var worst = min(light_score, shadows_score)
+	var scoring = 0.0
+	if diff == 0:
+		scoring = 2.0 * worst + best
+	else:
+		scoring = 1.0/float(diff) * float(diff)
+		scoring = (scoring * worst) + best
+	balance_score = int(scoring)
+
+func score(objects_count: int = 1):
+	reduce_delta = 0.0
+	reset_delta = true
+	for n in objects_count:
+		if mode == Mode.Light:
+			light_score += int(base_score * score_multiplier)
+		else:
+			shadows_score += int(base_score * score_multiplier)
+		calc_balance_score()
+		score_multiplier += score_multiplier_increment
+		if score_multiplier > max_score_multiplier: score_multiplier = max_score_multiplier
+	score_update()
+
+func set_auto_launch(enable : bool):
+	auto_launch = enable
+	auto_launch_off.visible = !auto_launch
+	auto_launch_on.visible = auto_launch
+
+func toggle_auto_launch():
+	set_auto_launch(!auto_launch)
+
+func wrong():
+	reduce_delta += 0.12
+	check_score_multiplier(0.0)
+	score_update()
+
+func check_score_multiplier(delta):
+	if score_multiplier < 2.0 or reset_delta:
+		multi_timer.scale.x = 1.0
+		multi_timer.visible = false
+		reduce_delta = 0.0
+		reset_delta = false
+		return
+	multi_timer.visible = true
+	reduce_delta += delta / REDUCE_MULTIPLIER_DURATION
+	multi_timer.scale.x = 1.0 - reduce_delta
+	if reduce_delta >= 1.0:
+		reduce_delta = 0.0
+		score_multiplier = 1.0
+	score_update()
+# end extra
+
+# input events
+func handle_lane_input(lane: int, modifier: bool = false):
+	if selected_object == null: return
+	selected_object.lane = lane
+	if auto_launch != modifier:
+		launch_selected()
+	else:
+		update_selected()
+
+func handle_move_input(direction: int):
+	move_selected(direction)
+
+func _input(event):
+	for n in grid_count_x:
+		if event.is_action_pressed("Lane" + str(n)): handle_lane_input(n, input_modifier)
+	#
+	if event.is_action_pressed("MoveLeft"): handle_move_input(-1)
+	elif event.is_action_pressed("MoveRight"): handle_move_input(1)
+	#
+	if event.is_action_pressed("Launch"): launch_selected()
+	if event.is_action_pressed("ToggleAutoLaunch"): toggle_auto_launch()
+	if event.is_action_pressed("ToggleMode"): toggle_mode()
+	
+	# TODO: remove these later
+	if event.is_action_pressed("DebugPlus"):
+		score(1)
+	if event.is_action_pressed("DebugMinus"):
+		toggle_mode()
+		score(1)
+
+# signals
+func _on_add_falling_pressed():
+	add_falling_line()
+
+func _on_move_pressed():
+	move_falling()
+
+func _on_launch_pressed():
+	launch_selected()
+
+func _on_move_left_pressed():
+	move_selected(MoveDirection.Left)
+
+func _on_move_right_pressed():
+	move_selected(MoveDirection.Right)
+
+func _on_lane_0_pressed():
+	handle_lane_input(0)
+
+func _on_lane_1_pressed():
+	handle_lane_input(1)
+
+func _on_lane_2_pressed():
+	handle_lane_input(2)
+
+func _on_lane_3_pressed():
+	handle_lane_input(3)
+
+func _on_lane_4_pressed():
+	handle_lane_input(4)
+
+func _on_toggle_pressed():
+	toggle_auto_launch()
+
+func _on_mode_pressed():
+	toggle_mode()
+
+func _on_give_up_pressed():
+	trigger_game_over(null)
